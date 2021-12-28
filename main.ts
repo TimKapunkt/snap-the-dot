1e3c9eea3fc2882688ab6a3fdabcbdeb4720cdcb
TimKapunkt <96684814+TimKapunkt@users.noreply.github.com>
2021-12-28 13:53:52 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,41 @@ input.onButtonPressed(Button.A, function () {
     if (sprite.get(LedSpriteProperty.X) == 2) {
         game.addScore(1)
     } else {
+        if (game.score() < 2) {
+            basic.showLeds(`
+                # # . # #
+                # # . # #
+                . . . . .
+                . # # # .
+                # . . . #
+                `)
+            basic.pause(100)
+            sprite.delete()
+        } else {
+            basic.showLeds(`
+                # # . # #
+                # # . # #
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+            basic.pause(100)
+            sprite.delete()
+        }
         game.gameOver()
     }
 })
+input.onGesture(Gesture.Shake, function () {
+    game.resume()
+})
+input.onButtonPressed(Button.B, function () {
+    game.pause()
+})
 let sprite: game.LedSprite = null
 sprite = game.createSprite(2, 2)
+let speed = 100
 basic.forever(function () {
     sprite.move(1)
     sprite.ifOnEdgeBounce()
-    basic.pause(200)
+    basic.pause(speed)
 })
